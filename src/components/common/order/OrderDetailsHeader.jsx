1b4c9209d03ccdabed27aa6f2163f675e5e606db
iammeskat <meskatcse@gmail.com>
2022-08-03 15:28:07 +0600

    print invoice

--- src/components/common/order/OrderDetailsHeader.jsx
+++ src/components/common/order/OrderDetailsHeader.jsx
@@ -6,23 +6,23 @@ const OrderDetailsHeader = (props) => {
       </h2>
       <button
         onClick={() => window.print()}
-        className="flex items-center text-white bg-teal-500 rounded space-x-1 py-1 px-1.5 border print:hidden"
+        className="flex items-center text-white bg-blue-500 hover:bg-blue-600 rounded space-x-1 py-1 px-1.5 border print:hidden"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4"
+          className="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
+          strokeWidth={2}
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+            d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
           />
         </svg>
-        <span className="text-sm">Invoice</span>
+        <span className="font-medium">INVOICE</span>
       </button>
     </div>
   );
